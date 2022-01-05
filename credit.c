cffad4d0baeee79f8c8664c6f2d5442428325648
Alejandro Franco <Alejandro.franco@teaminternational.com>
2022-01-05 15:44:04 -0500

    Added Validation is working for only one type of credit card

--- credit.c
+++ credit.c
@@ -8,24 +8,49 @@ long get_credit_car_number(void);
 int main(void)
 {
     long number = get_credit_car_number();
-    int number_0 = fmod(number / pow(10,0),10);
-    int number_1 = fmod(number / pow(10,1),10);
-    int number_2 = fmod(number / pow(10,2),10);
-    int number_3 = fmod(number / pow(10,3),10);
-    int number_4 = fmod(number / pow(10,4),10);
-    int number_5 = fmod(number / pow(10,5),10);
-    int number_6 = fmod(number / pow(10,6),10);
-    int number_7 = fmod(number / pow(10,7),10);
-    int number_8 = fmod(number / pow(10,8),10);
-    int number_9 = fmod(number / pow(10,9),10);
-    int number_10 = fmod(number / pow(10,10),10);
-    int number_11 = fmod(number / pow(10,11),10);
-    int number_12= fmod(number / pow(10,12),10);
-    int number_13= fmod(number / pow(10,13),10);
-    int number_14 = fmod(number / pow(10,14),10);
-    int number_15= fmod(number / pow(10,15),10);
+    int suma = 0;
+    int suma2 = 0; 
+    int result = 0;
+    int valid = 0;
+    
+    for (int i = 1; i < 16; i+=2)
+    {   
+        int digit = fmod(number / pow(10,i), 10) * 2;
 
-    printf("\n%i and %i and %i", number_0, number_1, number_14);
+        if (digit > 9)
+        {
+            digit = (digit % 10) + 1;
+        }
+        suma = suma + digit;
+        //printf("%i ", digit);
+    }
+    //printf("\n%i\n", suma);
+
+    for (int j = 0; j < 16; j+=2)
+    {
+        int digit2 = fmod(number / pow(10,j), 10);
+
+        if (digit2 > 9)
+        {
+            digit2 = (digit2 % 10) + 1;
+        }
+        //printf("%i ",digit2);
+        suma2 = suma2 + digit2;
+    }
+    //printf("\n%i\n", suma2);
+
+    result = suma + suma2;
+    valid = result % 10;
+
+    if (valid == 0)
+    {
+        printf("Valid Card\n\n");
+    }
+    else
+    {
+        printf("Invalid Card\n\n");
+    }
+    
 }
 
 long get_credit_car_number(void)
